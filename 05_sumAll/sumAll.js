78818bc2ef29f370615c85d58c98138d4a421984
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-20 17:26:01 -0300

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 > 0 && typeof num1 === 'number' && num2 > 0 && typeof num2 === 'number') {
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        } else if (num1 > num2) {
+            for (let i = num1; i >= num2; i--) {
+                sum += i;
+            }
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
