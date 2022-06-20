5c712e69a23c7d2531108deab37168a499b48a5a
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-20 16:26:51 -0300

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedItems) {
+    for (const item of removedItems) {
+        const index = array.indexOf(item);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
