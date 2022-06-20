e5346774dc3f1182a3ff607ba2b3a16ae4dd8edb
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-20 14:48:38 -0300

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splittedArray = string.split('');
+    let reversedArray = splittedArray.reverse();
+    let reversedString = reversedArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
