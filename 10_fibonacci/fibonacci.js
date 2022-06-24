618988759524524b4f002a452cdee0609f98c618
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-24 16:32:07 -0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sequence = [1, 1];
+    
+    for (let i = 2; i < num; i++) {
+        let number = sequence[i - 1] + sequence[i - 2];
+        sequence.push(number);
+    }
 
+    if (num > 0) {
+        return sequence[num - 1];
+    } else {
+        return 'OOPS';
+    }
 };
 
 // Do not edit below this line
