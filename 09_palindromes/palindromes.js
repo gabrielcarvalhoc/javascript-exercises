d24faa03da8868c809b7785f5a7b2d3966b2e413
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-24 15:45:10 -0300

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let modifiedString = string.replace(/\s|[.,!]/g, '').toLowerCase()
+    let reverseString = modifiedString.split('').reverse().join('');
 
+    return modifiedString === reverseString ? true : false;
 };
 
 // Do not edit below this line
