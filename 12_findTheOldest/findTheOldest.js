3d89142db248f26d23658e9c62649a1091f12ba7
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-27 15:00:25 -0300

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    for (let i = 0; i < people.length; i++) {
+        if (people[i]['yearOfDeath']) {
+            people[i]['age'] = people[i]['yearOfDeath'] - people[i]['yearOfBirth'];
+        } else {
+            people[i]['currentYear'] = new Date().getFullYear();
+            people[i]['age'] = people[i]['currentYear'] - people[i]['yearOfBirth'];
+        }
+    }
 
+    people.sort((a, b) => {
+        if (a.age > b.age) {
+            return -1;
+        }
+        if (a.age < b.age) {
+            return 1;
+        }
+        return 0;
+    })
+
+    return people[0];
 };
 
 // Do not edit below this line
